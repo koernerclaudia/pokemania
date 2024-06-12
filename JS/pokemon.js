a04549db2a26bf3f4a88d25a08f2bf9fe63af745
Claudia Koerner <claudia.koerner@gmail.com>
2024-06-12 15:36:05 +0200

    Update pokemon.js
    Set up a variable for a list of Pokemons and 5 sets of data of Pokemons including name, height, weight, and type.

--- JS/pokemon.js
+++ JS/pokemon.js
@@ -1,4 +1,7 @@
-alert('Hello world');
-
-var favouriteFood ='ketchup-filled Watchog Buns';
-document.write(favouriteFood)
+let PokemonList = [
+    { name: 'Bulbasaur', height: 0.7, type:['grass', 'poison'], weight: 6.9},
+    { name: 'Pikachu', height: 0.4, type:'electric', weight: 6},
+    { name: 'Charizard', height: 1.7, type:['fire', 'flying'], weight: 90.5},
+    { name: 'Squirtle', height: 0.5, type:'water', weight: 9},
+    { name: 'Jigglypuff', height: 0.5, type:['normal', 'fairy'], weight: 5.5},
+];
