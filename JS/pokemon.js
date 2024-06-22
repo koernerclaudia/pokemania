1ab04edd7098257a6f1b1fdcf9630c8b57b0808e
Claudia Koerner <claudia.koerner@gmail.com>
2024-06-22 10:37:40 +0200

    updated with exercise part 1 (1.5)

--- JS/pokemon.js
+++ JS/pokemon.js
@@ -1,38 +1,92 @@
-let PokemonList = [
+let pokemonRepository = (function () {
+    let PokemonList = [
     { name: 'Bulbasaur', height: 0.7, type:['grass', 'poison'], weight: 6.9},
     { name: 'Pikachu', height: 0.4, type:'electric', weight: 6},
-    { name: 'Charizard', height: 1.7, type:['fire', 'flying'], weight: 90.5},
+    { name: 'Charizard', height: 1.7, type:['fire', ' flying'], weight: 90.5},
     { name: 'Squirtle', height: 0.5, type:'water', weight: 9},
     { name: 'Jigglypuff', height: 0.5, type:['normal', 'fairy'], weight: 5.5},
-    { name: 'Eevee', height: 1.2, type:[' ', ' '], weight: 0}
-];
+    { name: 'Eevee', height: 1.2, type:[' ', ' '], weight: 0}];
+
+    return {
+              add: function(pokemon) {
+                pokemonList.push(pokemon);
+              },
+              getAll: function() {
+                return pokemonList;
+              }
+            };
+          })();
+          
+          console.log(pokemonRepository.getAll()); // []
+          pokemonRepository.add({ name: 'Pikachu' });
+          console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
+
+
+
+//   function myPokemons(pokemon) {
+//     console.log(pokemon.name + ' is ' + pokemon.height + ' tall, is of type: ' + pokemon.type + ' and weighs ' + pokemon.weight + 'kg.');
+//     document.write(pokemon.name + " is " + pokemon.height + " tall, is of type: " + pokemon.type + " and weighs " + pokemon.weight + "kg.<br>");
+//   }
+//   PokemonList.forEach(myPokemons);
+
+
+//   let pokemonRepository = (function () {
+//     let pokemonList = []; // empty array
+  
+//     return {
+//       add: function(pokemon) {
+//         pokemonList.push(pokemon);
+//       },
+//       getAll: function() {
+//         return pokemonList;
+//       }
+//     };
+//   })();
+  
+//   console.log(pokemonRepository.getAll()); // []
+//   pokemonRepository.add({ name: 'Pikachu' });
+//   console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
+
+
+//----------------------------------------------
+
+  // PokemonList.forEach(function(pokemon) {
+//     console.log(pokemon.name + ' is ' + pokemon.height + ' tall.<br>');
+//     document.write(pokemon.name + ' is ' + pokemon.height + ' tall.<br>');
+//   });
+
 // Setting up the information for a number of Pokemons with their characteristics of name, height, weight and type. 
 // Set up with a variable PokeMonList and as an array of objects.
 
-let bigThreshold = 1.0;
-let bigLabelGiven = false;
-// Setting up variables for the height threshold, proclaiming that if a Pokemon is taller than 1m it is big.
-// if it is smaller than 1m, the proclamaitin below will not be written/ logged.
+// let bigThreshold = 1.0;
+// let bigLabelGiven = false;
+// // Setting up variables for the height threshold, proclaiming that if a Pokemon is taller than 1m it is big.
+// // if it is smaller than 1m, the proclamaitin below will not be written/ logged.
 
-for (let i = 0; i < PokemonList.length; i++) {
-    let pokemon = PokemonList[i];
-    let output = "<div class='pokemon-item'><span class='pokemon-name'>" + pokemon.name + "</span> (height: <span class='pokemon-height'>" + pokemon.height + "m</span>)";
+// for (let i = 0; i < PokemonList.length; i++) {
+//     let pokemon = PokemonList[i];
+//     let output = "<div class='pokemon-item'><span class='pokemon-name'>" + pokemon.name + "</span> (height: <span class='pokemon-height'>" + pokemon.height + "m</span>)";
+
+// output += "</div>";
+// document.write(output + "<br>");
+// }
+
+
 
     // The for loop to output the list of Pokemons and their characteristics using the integer (i) to list all of them in the object array. (using .length)
 
-    if (pokemon.height > bigThreshold && !bigLabelGiven) {
-        output += " - <span class='big-note'>Wow, that’s big!</span>";
-        bigLabelGiven = true;
-    }
+    // if (pokemon.height > bigThreshold && !bigLabelGiven) {
+    //     output += " - <span class='big-note'>Wow, that’s big!</span>";
+    //     bigLabelGiven = true;
+    // }
 // Conditional statement to call the Pokemon(s) with a height larger than 1m including some CSS to give this another colour.
 
-output += "</div>";
-document.write(output + "<br>");
-}
 
 
 
-// =================
+
+// =================================================================
+
 // Other possible solution listed from CF:
 
 // let pokemonList = [
